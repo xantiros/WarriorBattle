8fd37e54d953b857e0a0b165686814b65402b3c8
Xantiros <xantiros@gmail.com>
2018-06-11 14:33:27 +0200

    IWeaponRepository

---
+++ WarriorBattle.Core/Repositories/IWeaponRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarriorBattle.Core.Domain;
+
+namespace WarriorBattle.Core.Repositories
+{
+    public interface IWeaponRepository
+    {
+        Weapon Get(Guid id);
+        IEnumerable<Weapon> GetAll();
+        void Add(Weapon weapon);
+        void Remove(Guid id);
+    }
+}
